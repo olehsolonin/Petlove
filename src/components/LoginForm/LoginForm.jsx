f925befa8d63efedf0990e88677f567121e452be
Oleh <62122094+olehsolonin@users.noreply.github.com>
2025-03-06 23:16:46 +0200

    added validation on formik forms

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,6 +1,30 @@
 import { Formik, Form, Field } from "formik";
 import css from "./LoginForm.module.css";
 import { NavLink } from "react-router-dom";
+import * as Yup from "yup";
+
+const initialValues = {
+  email: "",
+  userpassword: "",
+};
+
+const handleSubmit = (values, actions) => {
+  console.log("Form Submitted", values);
+  actions.resetForm();
+};
+
+const FeedbackSchema = Yup.object().shape({
+  email: Yup.string()
+    .matches(
+      /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/,
+      "Invalid email format"
+    )
+    .required("Email is required"),
+  userpassword: Yup.string()
+    .min(7, "Too short")
+    .max(256, "Too long")
+    .required("Required"),
+});
 
 export default function LoginForm() {
   return (
@@ -9,23 +33,40 @@ export default function LoginForm() {
       <p className={css.loginText}>
         Welcome! Please enter your credentials to login to the platform:
       </p>
-      <Formik initialValues={{}} onSubmit={() => {}}>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        validationSchema={FeedbackSchema}
+      >
         <Form className={css.formFields}>
           <div className={css.emailTextFields}>
-            <Field type="text" name="username" className={css.fields} />
-            <Field type="email" name="email" className={css.fields} />
+            <Field
+              type="email"
+              name="email"
+              className={css.fields}
+              placeholder="Email"
+            />
+
+            <Field
+              type="password"
+              name="userpassword"
+              className={css.fields}
+              placeholder="Password"
+            />
           </div>
           <div className={css.submitButton}>
             {" "}
             <button type="submit" className={css.button}>
               Log In
             </button>
-            <p className={css.footerText}>Don’t have an account? </p>
-            <span className={css.registerLink}>
-              <nav className={css.nav}>
-                <NavLink to="/register">Register</NavLink>
-              </nav>
-            </span>
+            <div className={css.loginTextContainer}>
+              <p className={css.footerText}>Don’t have an account? </p>
+              <span className={css.registerLink}>
+                <nav className={css.nav}>
+                  <NavLink to="/register">Register</NavLink>
+                </nav>
+              </span>
+            </div>
           </div>
         </Form>
       </Formik>
--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -2,6 +2,40 @@ import css from "./Registration.module.css";
 import Header from "../Header/Header.jsx";
 import PetBlockDog from "../PetBlockDog/PetBlockDog.jsx";
 import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+import { NavLink } from "react-router-dom";
+
+const initialValues = {
+  username: "",
+  email: "",
+  userpassword: "",
+  confirmPassword: "",
+};
+
+const handleSubmit = (values, actions) => {
+  console.log("Form Submitted", values);
+  actions.resetForm();
+};
+
+const FeedbackSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  email: Yup.string()
+    .matches(
+      /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/,
+      "Invalid email format"
+    )
+    .required("Email is required"),
+  userpassword: Yup.string()
+    .min(7, "Too short")
+    .max(256, "Too long")
+    .required("Required"),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref("userpassword"), null], "Passwords must match") // Проверяем, что пароли совпадают
+    .required("Required"),
+});
 
 export default function Registration() {
   return (
@@ -21,7 +55,11 @@ export default function Registration() {
         </div>
         <div className={css.formButtonContainer}>
           <div className={css.containerBox}>
-            <Formik initialValues={{}} onSubmit={() => {}}>
+            <Formik
+              initialValues={initialValues}
+              onSubmit={handleSubmit}
+              validationSchema={FeedbackSchema}
+            >
               <Form>
                 <div className={css.formForm}>
                   <Field
@@ -30,24 +68,29 @@ export default function Registration() {
                     className={css.formStyle}
                     placeholder="Name"
                   />
+                  {/* <ErrorMessage name="username" component="span" /> */}
+
                   <Field
                     type="email"
                     name="email"
                     className={css.formStyle}
                     placeholder="Email"
                   />
+
                   <Field
                     type="password"
                     name="userpassword"
                     className={css.formStyle}
                     placeholder="Password"
                   />
+
                   <Field
                     type="password"
                     name="confirmPassword"
                     className={css.formStyle}
                     placeholder="Confirm password"
                   />
+                  {/* <ErrorMessage name="confirmPassword" component="span" /> */}
                 </div>
                 <button type="submit" className={css.registerButton}>
                   Register
@@ -56,10 +99,13 @@ export default function Registration() {
             </Formik>
           </div>
           <div className={css.registerTextContainer}>
-            <p className={css.registerText}>
-              Already have an account?{" "}
-              <span className={css.spanText}>Login</span>
-            </p>
+            <p className={css.registerText}> Already have an account? </p>
+
+            <span className={css.spanText}>
+              <nav className={css.nav}>
+                <NavLink to="/login">Login</NavLink>
+              </nav>
+            </span>
           </div>
         </div>
       </div>
