301a57dcf0be90fca34a63a068805d340b4e7e3f
Oleh <62122094+olehsolonin@users.noreply.github.com>
2025-05-08 01:07:50 +0300

    make new formik form

--- src/components/NoticesFilters/NoticesFilters.jsx
+++ src/components/NoticesFilters/NoticesFilters.jsx
@@ -4,10 +4,11 @@ import { useEffect } from "react";
 import { fetchCategories, fetchSex, fetchSpecies } from "../../fetchReq";
 import { useSelector, useDispatch } from "react-redux";
 import { addCategories, addSex, addSpecies } from "../../redux/noticesSlice.js"; // <-- правильный экшен
+import { useId } from "react";
+import { Formik, Form, Field } from "formik";
 
 export default function NoticesFilters() {
   const dispatch = useDispatch();
-  //   const friends = useSelector((state) => state.searchFriends.friends);
 
   useEffect(() => {
     const getCatalog = async () => {
@@ -41,11 +42,50 @@ export default function NoticesFilters() {
     getSpecies();
   }, [dispatch]);
 
+  const categories = useSelector((state) => state.notices.categories);
+
+  const initialValues = {
+    username: "",
+    email: "",
+    message: "",
+    level: "good",
+  };
+
+  const nameFieldId = useId();
+  const emailFieldId = useId();
+  const msgFieldId = useId();
+  const levelFieldId = useId();
+
+  const handleSubmit = (values, actions) => {
+    console.log(values);
+    actions.resetForm();
+  };
+
   return (
     <div className={css.noticesFiltersContainer}>
       <SearchField />
       <div>
-        <h1>Salam</h1>
+        <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+          <Form>
+            <label htmlFor={nameFieldId}>Username</label>
+            <Field type="text" name="username" id={nameFieldId} />
+
+            <label htmlFor={emailFieldId}>Email</label>
+            <Field type="email" name="email" id={emailFieldId} />
+
+            <label htmlFor={msgFieldId}>Message</label>
+            <Field as="textarea" name="message" id={msgFieldId} rows="5" />
+
+            <label htmlFor={levelFieldId}>Service satisfaction level</label>
+            <Field as="select" name="level" id={levelFieldId}>
+              <option value="good">Good</option>
+              <option value="neutral">Neutral</option>
+              <option value="bad">Bad</option>
+            </Field>
+
+            <button type="submit">Submit</button>
+          </Form>
+        </Formik>
       </div>
     </div>
   );
