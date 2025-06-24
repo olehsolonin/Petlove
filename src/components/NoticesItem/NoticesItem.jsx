ee0a52e2eae2a8d3ae0d79bc1830c827223238e7
Oleh <62122094+olehsolonin@users.noreply.github.com>
2025-06-25 00:03:19 +0300

    add new Notices components

---
+++ src/components/NoticesItem/NoticesItem.jsx
@@ -0,0 +1,9 @@
+import css from "./NoticesItem.module.css";
+
+export default function NoticesItem() {
+  return (
+    <div>
+      <p>Salam brat</p>
+    </div>
+  );
+}
---
+++ src/components/NoticesList/NoticesList.jsx
@@ -0,0 +1,5 @@
+import css from "./NoticesList.module.css";
+import NoticesItem from "../NoticesItem/NoticesItem.jsx";
+import { useSelector } from "react-redux";
+
+export default function NoticesList() {}
