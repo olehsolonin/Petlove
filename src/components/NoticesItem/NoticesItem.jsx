b79c9028f6f36fd19d9369945688e6867afa68e7
Oleh <62122094+olehsolonin@users.noreply.github.com>
2025-07-04 01:07:31 +0300

    created rendering of components NoticesItem

--- src/components/NoticesItem/NoticesItem.jsx
+++ src/components/NoticesItem/NoticesItem.jsx
@@ -1,18 +1,34 @@
 import css from "./NoticesItem.module.css";
+import { FaStar } from "react-icons/fa";
 
 export default function NoticesItem({ notice }) {
-  const { imgUrl, title, date, text, url } = notice; // Деструктуризируем внутри
+  const { imgURL, title, date, comment, url, popularity } = notice; // Деструктуризируем внутри
   //   console.log(title);
   return (
     <div className={css.noticesItemContainer}>
       <div className={css.imageContainer}>
         {" "}
-        <img src={imgUrl} alt={title} className={css.imageSettings} />
+        <img src={imgURL} alt={title} className={css.imageSettings} />
       </div>
       <div className={css.descriptionContainer}>
         <div className={css.titleTextWrap}>
-          <h2 className={css.title}>{title}</h2>
-          <p className={css.text}>{text}</p>
+          <div className={css.detailsContainer}>
+            <div className={css.titlePopularityWrap}>
+              <h2 className={css.title}>{title}</h2>
+              <div className={css.popularityBlock}>
+                <div className={css.starWrap}>
+                  {" "}
+                  <FaStar className={css.star} />
+                </div>
+                {popularity}
+              </div>
+            </div>
+            {/* <p className={css.text}>{text}</p> */}
+            <div className={css.detailsTextWrap}></div>
+          </div>
+          <div className={css.commentContainer}>
+            <h2>{comment}</h2>
+          </div>
         </div>
         <div className={css.dateLinkWrap}>
           <p className={css.dateMarker}>{date}</p>
