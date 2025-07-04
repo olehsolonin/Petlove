3c87f68189f9e62335e5df05f48b99f4f65922c2
Oleh <62122094+olehsolonin@users.noreply.github.com>
2025-07-05 01:38:37 +0300

    added some pices of notices component

--- src/components/NoticesItem/NoticesItem.jsx
+++ src/components/NoticesItem/NoticesItem.jsx
@@ -1,8 +1,22 @@
 import css from "./NoticesItem.module.css";
 import { FaStar } from "react-icons/fa";
+import { CiHeart } from "react-icons/ci";
 
 export default function NoticesItem({ notice }) {
-  const { imgURL, title, date, comment, url, popularity } = notice; // Деструктуризируем внутри
+  const {
+    imgURL,
+    title,
+    date,
+    comment,
+    url,
+    popularity,
+    birthday,
+    name,
+    sex,
+    species,
+    category,
+    price,
+  } = notice; // Деструктуризируем внутри
   //   console.log(title);
   return (
     <div className={css.noticesItemContainer}>
@@ -24,17 +38,49 @@ export default function NoticesItem({ notice }) {
               </div>
             </div>
             {/* <p className={css.text}>{text}</p> */}
-            <div className={css.detailsTextWrap}></div>
+            <div className={css.detailsTextWrap}>
+              <div className={css.detailsSmallBlocks}>
+                <p className={css.detailsTitle}>Name</p>
+                <p className={css.fetchDetails}>{name}</p>
+              </div>
+              <div className={css.detailsSmallBlocks}>
+                <p className={css.detailsTitle}>Birthday</p>
+                <p className={css.fetchDetails}>{birthday}</p>
+              </div>
+              <div className={css.detailsSmallBlocks}>
+                <p className={css.detailsTitle}>Sex</p>
+                <p className={css.fetchDetails}>{sex}</p>
+              </div>
+              <div className={css.detailsSmallBlocks}>
+                <p className={css.detailsTitle}>Species</p>
+                <p className={css.fetchDetails}>{species}</p>
+              </div>
+              <div className={css.detailsSmallBlocks}>
+                <p className={css.detailsTitle}>Category</p>
+                <p className={css.fetchDetails}>{category}</p>
+              </div>
+            </div>
           </div>
           <div className={css.commentContainer}>
-            <h2>{comment}</h2>
+            <h2 className={css.commentText}>{comment}</h2>
           </div>
         </div>
-        <div className={css.dateLinkWrap}>
-          <p className={css.dateMarker}>{date}</p>
-          <a href={url} className={css.link}>
-            Read more
-          </a>
+        <div className={css.priceLinkWrap}>
+          <div>
+            {category === "free" ? (
+              <p className={css.priceCategory}>{category}</p>
+            ) : (
+              <p className={css.priceCategory}>${price}</p>
+            )}
+          </div>
+          <div className={css.buttonLikeBlock}>
+            <button type="button" className={css.learnButton}>
+              Learn more
+            </button>
+            <button type="button" className={css.likeButton}>
+              <CiHeart className={css.heartButton} />
+            </button>
+          </div>
         </div>
       </div>
     </div>
