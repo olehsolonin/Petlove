f68cabaed91a5fecb9e392202e1cedea9fee7cbe
Oleh <62122094+olehsolonin@users.noreply.github.com>
2025-12-28 19:40:31 +0000

    fix formik req

--- src/components/NoticesFilters/NoticesFilters.jsx
+++ src/components/NoticesFilters/NoticesFilters.jsx
@@ -1,238 +1,252 @@
-import SearchField from "../SearchField/SearchField";
-import css from "./NoticesFilters.module.css";
-import { useEffect } from "react";
+import SearchField from '../SearchField/SearchField';
+import css from './NoticesFilters.module.css';
+import { useEffect } from 'react';
 import {
-  fetchCategories,
-  fetchSex,
-  fetchSpecies,
-  fetchLocations,
-  fetchAllNotices,
-} from "../../fetchReq";
-import { useSelector, useDispatch } from "react-redux";
+    fetchCategories,
+    fetchSex,
+    fetchSpecies,
+    fetchLocations,
+    fetchAllNotices,
+} from '../../fetchReq';
+import { useSelector, useDispatch } from 'react-redux';
 import {
-  addCategories,
-  addSex,
-  addSpecies,
-  addLocations,
-  addParams,
-  addResults,
-} from "../../redux/noticesSlice.js";
-import { useId } from "react";
-import { Formik, Form, Field } from "formik";
-import Select from "react-select";
-import LocationSelect from "../Select/Select.jsx";
+    addCategories,
+    addSex,
+    addSpecies,
+    addLocations,
+    addParams,
+    addResults,
+} from '../../redux/noticesSlice.js';
+import { useId } from 'react';
+import { Formik, Form, Field } from 'formik';
+import Select from 'react-select';
+import LocationSelect from '../Select/Select.jsx';
 
 export default function NoticesFilters() {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  useEffect(() => {
-    const getCatalog = async () => {
-      try {
-        const res = await fetchCategories();
-        dispatch(addCategories(res));
-      } catch (error) {
-        console.error("Помилка завантаження категорій:", error);
-      }
-    };
-    const getSex = async () => {
-      try {
-        const res = await fetchSex();
-        dispatch(addSex(res));
-      } catch (error) {
-        console.error("Помилка завантаження статі:", error);
-      }
-    };
+    useEffect(() => {
+        const getCatalog = async () => {
+            try {
+                const res = await fetchCategories();
+                dispatch(addCategories(res));
+            } catch (error) {
+                console.error('Помилка завантаження категорій:', error);
+            }
+        };
+        const getSex = async () => {
+            try {
+                const res = await fetchSex();
+                dispatch(addSex(res));
+            } catch (error) {
+                console.error('Помилка завантаження статі:', error);
+            }
+        };
 
-    const getSpecies = async () => {
-      try {
-        const res = await fetchSpecies();
-        dispatch(addSpecies(res));
-      } catch (error) {
-        console.error("Помилка завантаження:", error);
-      }
-    };
+        const getSpecies = async () => {
+            try {
+                const res = await fetchSpecies();
+                dispatch(addSpecies(res));
+            } catch (error) {
+                console.error('Помилка завантаження:', error);
+            }
+        };
 
-    const getLocations = async () => {
-      try {
-        const res = await fetchLocations();
-        dispatch(addLocations(res));
-      } catch (error) {
-        console.error("Помилка завантаження доступних локацій:", error);
-      }
-    };
+        const getLocations = async () => {
+            try {
+                const res = await fetchLocations();
+                dispatch(addLocations(res));
+            } catch (error) {
+                console.error('Помилка завантаження доступних локацій:', error);
+            }
+        };
 
-    getCatalog();
-    getSex();
-    getSpecies();
-    getLocations();
-  }, [dispatch]);
+        getCatalog();
+        getSex();
+        getSpecies();
+        getLocations();
+    }, [dispatch]);
 
-  const categories = useSelector((state) => state.notices.categories);
-  const sexOptions = useSelector((state) => state.notices.sex);
-  const speciesOptions = useSelector((state) => state.notices.species);
-  const locationsOptions = useSelector((state) => state.notices.locations);
+    const categories = useSelector((state) => state.notices.categories);
+    const sexOptions = useSelector((state) => state.notices.sex);
+    const speciesOptions = useSelector((state) => state.notices.species);
+    const locationsOptions = useSelector((state) => state.notices.locations);
 
-  const initialValues = {
-    keyword: "",
-    catalog: "",
-    sex: "",
-    species: "",
-    locations: null,
-    byPopularity: "",
-    byPrice: "",
-  };
-
-  const catalogId = useId();
-  const speciesFieldId = useId();
-  const sexFieldId = useId();
-
-  const handleSubmit = (values, actions) => {
-    const cleanedValues = {};
-    console.log(values);
-
-    for (const key in values) {
-      const value = values[key];
-      if (value !== "" && value !== null && value !== undefined) {
-        cleanedValues[key] = value;
-      }
-    }
-
-    console.log(cleanedValues);
-    dispatch(addParams(cleanedValues));
-    //  const res = fetchAllNotices(cleanedValues);
-    //  dispatch(addResults(res));
-
-    const getResults = async () => {
-      try {
-        const res = await fetchAllNotices(cleanedValues);
-        dispatch(addResults(res.results));
-      } catch (error) {
-        console.error("Помилка завантаження результатів:", error);
-      }
+    const initialValues = {
+        keyword: '',
+        category: '',
+        sex: '',
+        species: '',
+        locations: null,
+        byPopularity: '',
+        byPrice: '',
     };
 
-    getResults();
+    const categoryId = useId();
+    const speciesFieldId = useId();
+    const sexFieldId = useId();
 
-    console.log(values);
-    actions.resetForm();
-  };
-  //   console.log(typeof categories);
+    const handleSubmit = (values, actions) => {
+        const cleanedValues = {};
+        console.log(values);
 
-  return (
-    <div className={css.noticesFiltersContainer}>
-      <div>
-        <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-          {({ values, handleChange }) => (
-            <Form className={css.formWrapper}>
-              <div className={css.topFiltersWrapper}>
-                <SearchField
-                  name="keyword"
-                  value={values.keyword}
-                  onChange={handleChange}
-                  insideFormik={true}
-                />
-                <div className={css.catalogSexWrapper}>
-                  <Field
-                    as="select"
-                    name="catalog"
-                    id={catalogId}
-                    className={css.catalogSexSelects}
-                  >
-                    <option value="" disabled hidden>
-                      Category
-                    </option>
-                    <option value="all">Show all</option>
-                    {categories.map((category) => (
-                      <option key={category} value={category}>
-                        {category}
-                      </option>
-                    ))}
-                  </Field>
+        for (const key in values) {
+            const value = values[key];
+            if (value !== '' && value !== null && value !== undefined) {
+                cleanedValues[key] = value;
+            }
+        }
 
-                  <Field
-                    as="select"
-                    name="sex"
-                    id={sexFieldId}
-                    className={css.catalogSexSelects}
-                  >
-                    <option value="" disabled hidden>
-                      By gender
-                    </option>
-                    <option value="all">Show all</option>
-                    {sexOptions.map((sex) => (
-                      <option key={sex} value={sex}>
-                        {sex}
-                      </option>
-                    ))}
-                  </Field>
-                </div>
+        console.log(cleanedValues);
+        dispatch(addParams(cleanedValues));
+        //  const res = fetchAllNotices(cleanedValues);
+        //  dispatch(addResults(res));
 
-                <div className={css.typeWrapper}>
-                  <Field
-                    as="select"
-                    name="species"
-                    id={speciesFieldId}
-                    className={css.typeSelects}
-                  >
-                    <option value="" disabled hidden>
-                      By type
-                    </option>
-                    <option value="all">Show all</option>
-                    {speciesOptions.map((species) => (
-                      <option key={species} value={species}>
-                        {species}
-                      </option>
-                    ))}
-                  </Field>
-                </div>
-                <LocationSelect name="locations" />
-              </div>
-              <div className={css.line}></div>
-              <div className={css.bottomFiltersWrapper}>
-                <label className={css.labelButtonWrapper}>
-                  <Field
-                    type="radio"
-                    name="byPopularity"
-                    value="true"
-                    className={css.radioInput}
-                  />
-                  <span className={css.customButton}>Popular</span>
-                </label>
+        const getResults = async () => {
+            try {
+                const res = await fetchAllNotices(cleanedValues);
+                dispatch(addResults(res.results));
+            } catch (error) {
+                console.error('Помилка завантаження результатів:', error);
+            }
+        };
 
-                <label className={css.labelButtonWrapper}>
-                  <Field
-                    type="radio"
-                    name="byPopularity"
-                    value="false"
-                    className={css.radioInput}
-                  />
-                  <span className={css.customButton}>Unpopular</span>
-                </label>
+        getResults();
 
-                <label className={css.labelButtonWrapper}>
-                  <Field
-                    type="radio"
-                    name="byPrice"
-                    value="true"
-                    className={css.radioInput}
-                  />
-                  <span className={css.customButton}>Cheap</span>
-                </label>
+        console.log(values);
+        actions.resetForm();
+    };
+    //   console.log(typeof categories);
 
-                <label className={css.labelButtonWrapper}>
-                  <Field
-                    type="radio"
-                    name="byPrice"
-                    value="false"
-                    className={css.radioInput}
-                  />
-                  <span className={css.customButton}>Expensive</span>
-                </label>
-              </div>
-            </Form>
-          )}
-        </Formik>
-      </div>
-    </div>
-  );
+    return (
+        <div className={css.noticesFiltersContainer}>
+            <div>
+                <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+                    {({ values, handleChange }) => (
+                        <Form className={css.formWrapper}>
+                            <div className={css.topFiltersWrapper}>
+                                <SearchField
+                                    name="keyword"
+                                    value={values.keyword}
+                                    onChange={handleChange}
+                                    insideFormik={true}
+                                />
+                                <div className={css.categorySexWrapper}>
+                                    <Field
+                                        as="select"
+                                        name="category"
+                                        id={categoryId}
+                                        className={css.categorySexSelects}
+                                    >
+                                        <option value="" disabled hidden>
+                                            Category
+                                        </option>
+                                        <option value="all">Show all</option>
+                                        {categories.map((category) => (
+                                            <option
+                                                key={category}
+                                                value={category}
+                                            >
+                                                {category}
+                                            </option>
+                                        ))}
+                                    </Field>
+
+                                    <Field
+                                        as="select"
+                                        name="sex"
+                                        id={sexFieldId}
+                                        className={css.categorySexSelects}
+                                    >
+                                        <option value="" disabled hidden>
+                                            By gender
+                                        </option>
+                                        <option value="all">Show all</option>
+                                        {sexOptions.map((sex) => (
+                                            <option key={sex} value={sex}>
+                                                {sex}
+                                            </option>
+                                        ))}
+                                    </Field>
+                                </div>
+
+                                <div className={css.typeWrapper}>
+                                    <Field
+                                        as="select"
+                                        name="species"
+                                        id={speciesFieldId}
+                                        className={css.typeSelects}
+                                    >
+                                        <option value="" disabled hidden>
+                                            By type
+                                        </option>
+                                        <option value="all">Show all</option>
+                                        {speciesOptions.map((species) => (
+                                            <option
+                                                key={species}
+                                                value={species}
+                                            >
+                                                {species}
+                                            </option>
+                                        ))}
+                                    </Field>
+                                </div>
+                                <LocationSelect name="locations" />
+                            </div>
+                            <div className={css.line}></div>
+                            <div className={css.bottomFiltersWrapper}>
+                                <label className={css.labelButtonWrapper}>
+                                    <Field
+                                        type="radio"
+                                        name="byPopularity"
+                                        value="true"
+                                        className={css.radioInput}
+                                    />
+                                    <span className={css.customButton}>
+                                        Popular
+                                    </span>
+                                </label>
+
+                                <label className={css.labelButtonWrapper}>
+                                    <Field
+                                        type="radio"
+                                        name="byPopularity"
+                                        value="false"
+                                        className={css.radioInput}
+                                    />
+                                    <span className={css.customButton}>
+                                        Unpopular
+                                    </span>
+                                </label>
+
+                                <label className={css.labelButtonWrapper}>
+                                    <Field
+                                        type="radio"
+                                        name="byPrice"
+                                        value="true"
+                                        className={css.radioInput}
+                                    />
+                                    <span className={css.customButton}>
+                                        Cheap
+                                    </span>
+                                </label>
+
+                                <label className={css.labelButtonWrapper}>
+                                    <Field
+                                        type="radio"
+                                        name="byPrice"
+                                        value="false"
+                                        className={css.radioInput}
+                                    />
+                                    <span className={css.customButton}>
+                                        Expensive
+                                    </span>
+                                </label>
+                            </div>
+                        </Form>
+                    )}
+                </Formik>
+            </div>
+        </div>
+    );
 }
