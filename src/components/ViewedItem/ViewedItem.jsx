26ac76774a27e7152d7d9a726f875d3dbe03fdf7
Oleh <62122094+olehsolonin@users.noreply.github.com>
2025-12-28 17:31:19 +0000

    created viewedList of pets

---
+++ src/components/ViewedItem/ViewedItem.jsx
@@ -0,0 +1,143 @@
+import css from './ViewedItem.module.css';
+import { RiDeleteBin5Line } from 'react-icons/ri';
+import {
+    fetchRemoveFromFavourites,
+    fetchFullUserInfo,
+} from '../../fetchReq.js';
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { userDataNoticesFavorites } from '../../redux/userInfoSlice.js';
+import ModalNotice from '../ModalNotice/ModalNotice.jsx';
+import { fetchFullPetInfoById } from '../../fetchReq.js';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export default function ViewedItem({ data }) {
+    const dispatch = useDispatch();
+    const token = useSelector((state) => state.auth.token);
+
+    const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+
+    const [isNoticeModalOpen, setIsNoticeModalOpen] = useState(false);
+    const [details, setDetails] = useState(null);
+    const {
+        birthday,
+        title,
+        category,
+        comment,
+        createdAt,
+        imgURL,
+        location,
+        name,
+        popularity,
+        sex,
+        species,
+        updatedAt,
+        user,
+        _id,
+        price,
+    } = data;
+
+    //  const handleSubmit = async () => {
+    //      try {
+    //          await fetchRemoveFromFavourites(_id, token);
+    //          const fullUser = await fetchFullUserInfo(token);
+    //          dispatch(userDataNoticesFavorites(fullUser.noticesFavorites));
+    //          toast.success('Pet remove successfully', {
+    //              position: 'top-center',
+    //          });
+    //      } catch (error) {
+    //          console.error(error);
+    //      }
+    //  };
+
+    const handleLearnMore = async () => {
+        try {
+            const res = await fetchFullPetInfoById(_id, token);
+            setDetails(res);
+            setIsNoticeModalOpen(true);
+        } catch (err) {
+            console.log('Error loading details:', err);
+        }
+    };
+
+    return (
+        <div className={css.viewedPetItem}>
+            <div className={css.petImgContainer}>
+                <img src={imgURL} alt="petPhoto" className={css.petImg} />
+            </div>
+            <div className={css.fullPetInfoContainer}>
+                <div className={css.titleInfoBlock}>
+                    <div className={css.justInfo}>
+                        <div className={css.titlePopularityContainer}>
+                            <h3 className={css.petTitle}>{title}</h3>
+                            <p className={css.popularity}>❤️ {popularity}</p>
+                        </div>
+                        <div className={css.petDetails}>
+                            <div className={css.petDetailItem}>
+                                {' '}
+                                <p className={css.petDetailsInfo}>Name</p>
+                                <span className={css.petData}>{name}</span>
+                            </div>
+                            <div className={css.petDetailItem}>
+                                {' '}
+                                <p className={css.petDetailsInfo}>Birthday</p>
+                                <span className={css.petData}>{birthday}</span>
+                            </div>
+                            <div className={css.petDetailItem}>
+                                {' '}
+                                <p className={css.petDetailsInfo}>Sex</p>
+                                <span className={css.petData}>{sex}</span>
+                            </div>
+                            <div className={css.petDetailItem}>
+                                {' '}
+                                <p className={css.petDetailsInfo}>Species</p>
+                                <span className={css.petData}>{species}</span>
+                            </div>
+                            <div className={css.petDetailItem}>
+                                {' '}
+                                <p className={css.petDetailsInfo}>Category</p>
+                                <span className={css.petData}>{category}</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={css.petCommentContainer}>
+                        <p className={css.comment}>{comment}</p>
+                    </div>
+                </div>
+                <div className={css.priceLearnContainer}>
+                    <div>
+                        {price ? (
+                            <p className={css.petPrice}>${price}</p>
+                        ) : (
+                            <p className={css.petPrice}>free</p>
+                        )}
+                    </div>
+                    <div className={css.learnDeleteBtns}>
+                        <button
+                            className={css.learnMoreBtn}
+                            type="button"
+                            onClick={handleLearnMore}
+                        >
+                            Learn more
+                        </button>
+                        {/* <button
+                            className={css.deleteBtn}
+                            type="button"
+                            onClick={() => handleSubmit(_id)}
+                        >
+                            <RiDeleteBin5Line className={css.deleteIcon} />
+                        </button> */}
+                    </div>
+                </div>
+            </div>
+            {isNoticeModalOpen && details && (
+                <ModalNotice
+                    isOpen={isNoticeModalOpen}
+                    onClose={() => setIsNoticeModalOpen(false)}
+                    data={details}
+                />
+            )}
+        </div>
+    );
+}
--- src/components/ViewedList/ViewedList.jsx
+++ src/components/ViewedList/ViewedList.jsx
@@ -1,7 +1,25 @@
+import { useSelector } from 'react-redux';
+import css from './ViewedList.module.css';
+import ViewedItem from '../ViewedItem/ViewedItem.jsx';
+
 export const ViewedList = () => {
+    const viewed = useSelector((state) => state.userInfo.noticesViewed);
+    console.log('viewed из store:', viewed);
     return (
-        <div>
-            <p>Salam brat</p>
+        <div className={css.viewedListContainer}>
+            {viewed.length > 0 ? (
+                <ul className={css.viewedList}>
+                    {viewed.map((item) => {
+                        return (
+                            <li key={item._id} className={css.viewedListItem}>
+                                <ViewedItem data={item} />
+                            </li>
+                        );
+                    })}
+                </ul>
+            ) : (
+                <p>Вибачте, у Вас відсутні переглянуті оголошення</p>
+            )}
         </div>
     );
 };
