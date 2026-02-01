516a726f004562731ad366e6c28f36d539862833
Oleh <62122094+olehsolonin@users.noreply.github.com>
2026-02-01 21:02:24 +0000

    homePage fix

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,50 +1,40 @@
-import css from "./Home.module.css";
-import { Routes, Route, NavLink } from "react-router-dom";
+import css from './Home.module.css';
+import { Routes, Route, NavLink } from 'react-router-dom';
 // import Header from "../Header/Header.jsx";
-import homePhoto1x from "../../img/homePhoto-1x.jpg";
-import homePhoto2x from "../../img/homePhoto-2x.jpg";
-import { FaHeart } from "react-icons/fa6";
+import homePhoto1x from '../../img/homePhoto-1x.jpg';
+import homePhoto2x from '../../img/homePhoto-2x.jpg';
+import { FaHeart } from 'react-icons/fa6';
+import Header from '../Header/Header';
 
 export default function Home() {
-  return (
-    <div className={css.homeContainer}>
-      <div className={css.topContainer}>
-        <div>
-          <nav className={css.nav}>
-            <NavLink to="/home">
-              <h1 className={css.mainTitle}>
-                petl
-                <span className={css.heartIconContainer}>
-                  <FaHeart className={css.heartIcon} />
-                </span>
-                ve
-              </h1>
-            </NavLink>
-          </nav>
+    return (
+        <div className={css.homeContainer}>
+            <Header />
+            <div className={css.topContainer}>
+                <div className={css.textCoantainer}>
+                    <h2 className={css.titleText}>
+                        Take good <span>care</span> of your small pets
+                    </h2>
+                    <p className={css.descriptionText}>
+                        Choosing a pet for your home is a choice that is meant
+                        to enrich your life with immeasurable joy and
+                        tenderness.
+                    </p>
+                </div>
+            </div>
+            <div className={css.photoContainer}>
+                <picture>
+                    <source
+                        srcSet={`${homePhoto1x} 1x, ${homePhoto2x} 2x`}
+                        media="(min-width: 320px)"
+                    />
+                    <img
+                        src={homePhoto1x}
+                        alt="Красивая картинка"
+                        className={css.photoSettings}
+                    />
+                </picture>
+            </div>
         </div>
-        <div className={css.textCoantainer}>
-          <h2 className={css.titleText}>
-            Take good <span>care</span> of your small pets
-          </h2>
-          <p className={css.descriptionText}>
-            Choosing a pet for your home is a choice that is meant to enrich
-            your life with immeasurable joy and tenderness.
-          </p>
-        </div>
-      </div>
-      <div className={css.photoContainer}>
-        <picture>
-          <source
-            srcSet={`${homePhoto1x} 1x, ${homePhoto2x} 2x`}
-            media="(min-width: 320px)"
-          />
-          <img
-            src={homePhoto1x}
-            alt="Красивая картинка"
-            className={css.photoSettings}
-          />
-        </picture>
-      </div>
-    </div>
-  );
+    );
 }
